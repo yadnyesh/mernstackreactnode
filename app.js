64587af66f00b7747a3cbf201498483bf694cb7b
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-03-14 07:48:27 +0530

    connected to MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const port = process.env.PORT || 8000;
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("DB Connected"));
 
 app.get('/', (req, res) => {
     res.send("hello from node yet again");
 });
 
-const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
